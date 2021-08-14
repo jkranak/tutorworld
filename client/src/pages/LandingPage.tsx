294fec68dbbe7dccaebd98f8b99f596cebeed213
biancahpp <bianca_procopio@hotmail.com>
2021-08-14 10:13:13 -0600

    feat: button component

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -3,31 +3,33 @@ import { Link } from "react-router-dom";
 import { About } from "../components/About";
 import { LandingBody } from "../components/LandingBody";
 
-interface Props {
-  
-}
-
-export const LandingPage = (props: Props) => {
-
+export const LandingPage = () => {
   const [toggle, setToggle] = useState<string>('home');
 
   return (
     <div className="landing">
 
       <header className="landing__header">
-        {toggle === 'home' && <button onClick={() => setToggle('about')}>About Us</button> }
-        {toggle === 'about' && <button onClick={() => setToggle('home')}>Home</button>}
-        
-        <Link to={'/application'}>Apply to be a Tutor</Link>
-        <Link to={'/login'}>Login/Register</Link>
+        <div className="landing__header--left-box">
+          {toggle === 'home' && <a 
+          onClick={() => setToggle('about')}
+          className="btn btn--clear">
+          About Us
+          </a> }
+          {toggle === 'about' && <a 
+          onClick={() => setToggle('home')}
+          className="btn btn--clear">
+          Home
+          </a>}
+        </div>
+        <div className="landing__header--right-box">
+          <Link to={'/application'} className="btn btn--clear">Apply to be a Tutor</Link>
+          <Link to={'/login'} className="btn btn--blue">Login/Register</Link>
+        </div>
       </header>
 
-      {
-        toggle === 'home' && <LandingBody/>
-      }
-      {
-        toggle === 'about' && <About />
-      }
+      {toggle === 'home' && <LandingBody/>}
+      {toggle === 'about' && <About />}
     </div>
   )
 }

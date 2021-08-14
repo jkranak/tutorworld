c076eca481d605ea9f14e42055752ef915a3d4e5
biancahpp <bianca_procopio@hotmail.com>
2021-08-14 11:15:28 -0600

    updating branch

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,29 +1,13 @@
 import {useState} from 'react'
-import {UserLogin, emptyUserLogin} from '../Interfaces/User';
+import { LoginForm } from '../components/LoginForm'
+import { RegisterForm } from '../components/RegisterForm'
 
-interface Props {
-  
-}
-
-export const Login = (props: Props) => {
-  const [userLogin, setUserLogin] = useState(emptyUserLogin)
-
-  function handleChange (event: any) {
-    setUserLogin(user => ({...user, [event.target.name]: event.target.value}))
-  }
-  function handleSubmit () {}
-  const register = '/'
-
-return (
-<div>
-      <form onSubmit={handleSubmit}>
-        <label>Email</label>
-        <input type="text" id="email" name="email" required onChange={handleChange} value={userLogin.email}/>
-        <label>Password</label>
-        <input type="password" id="password" name="password" required onChange={handleChange} value={userLogin.password}/>
-        <button type="submit">Login</button>
-      </form>
-      <p>If you don't have an account, <a href={register}>Register</a></p>
+export const Login = () => {
+  const [toggle, setToggle] = useState<string>('login')
+  return (
+    <div className="login">
+      {toggle === 'login' && <LoginForm setToggle={setToggle}/>}
+      {toggle === 'register' && <RegisterForm setToggle={setToggle}/>}
     </div>
   )
 }

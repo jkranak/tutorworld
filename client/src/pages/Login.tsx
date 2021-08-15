97b64d6d4c9ce57c1e0f2be9916babaf896353a2
biancahpp <bianca_procopio@hotmail.com>
2021-08-14 18:40:07 -0600

    new pages added

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { useState } from 'react'
 import { LoginForm } from '../components/LoginForm'
 import { RegisterForm } from '../components/RegisterForm'
@@ -11,21 +10,4 @@ export const Login = () => {
       {toggle === 'register' && <RegisterForm setToggle={setToggle}/>}
     </div>
   )
-=======
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import { LoginForm } from "../components/LoginForm";
-import { RegisterForm } from "../components/RegisterForm";
-
-export const Login = () => {
-  const [toggle, setToggle] = useState<string>('login');
-
-  return (
-    <div className="login">
-      <Link to={'/'} className="btn btn--blue">Home</Link>
-      {toggle === 'login' ? <LoginForm setToggle={setToggle} /> :
-      <RegisterForm setToggle={setToggle}/>}
-    </div>
-  )
->>>>>>> 565702de4496a7b3ca7dc8cf7d613e801f25220d
 }

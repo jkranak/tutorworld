d316761928f427d2b70a2d55857b61900d8ffc02
biancahpp <bianca_procopio@hotmail.com>
2021-08-16 13:35:46 -0600

    feat: working on positioning of elemnts in dashboard

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -11,11 +11,31 @@ export const Dashboard = () => {
       <div className="dashboard__content">
         <Sidebar/>
         <div className="dashboard__content--display">
-          <h2>Unread Messages</h2>
-          <h2>Upcoming Sessions</h2>
-          {role === 'tutor' 
-          ? <h2>History</h2> 
-          : <h2>Favorite Users</h2>}
+          <div className="dashboard__content--display--top">
+            <div className="dashboard__content--display--info">
+              <h1 className="dashboard__content--display--title">Unread Messages</h1>
+              <div className="dashboard__content--display--top--box">
+                <p>message</p>
+              </div>
+            </div>
+            <div className="dashboard__content--display--info">
+              <h1 className="dashboard__content--display--title">Upcoming Sessions</h1>
+              <div className="dashboard__content--display--top--box">
+                  <p>message</p>
+              </div>
+            </div>
+          </div>
+          {role === 'tutor' ? 
+            <div className="dashboard__content--display--bottom">
+              <h1 className="dashboard__content--display--title">History</h1> 
+              <h1 className="dashboard__content--display--title">Earnings Owned</h1>
+            </div>
+            :
+            <div className="dashboard__content--display--bottom">
+              <h1 className="dashboard__content--display--title">History</h1> 
+              <h1 className="dashboard__content--display--title">Favorite Tutors</h1>
+            </div>  
+          }
         </div>
       </div>
     </div>

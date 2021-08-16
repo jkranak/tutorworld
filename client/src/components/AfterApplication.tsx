9a129e10993c92f5b2b4fc9a91756790c16c9acb
biancahpp <bianca_procopio@hotmail.com>
2021-08-16 12:03:56 -0600

    feat: after application basic design

--- client/src/components/AfterApplication.tsx
+++ client/src/components/AfterApplication.tsx
@@ -1,16 +1,22 @@
+import { useHistory } from 'react-router-dom';
 import mailIcon from '../assets/email.svg'
 
-interface Props {
-  
-}
+export const AfterApplication = () => {
+  const history = useHistory();
 
-export const AfterApplication = (props: Props) => {
-  
+  setTimeout(() => {
+    history.push('/')
+  }, 5000)
+
   return (
-    <div>
-     <h2>Application sent</h2>
-     <h3>We are reviewing your application. We will email you soon.</h3>
-     <img src={mailIcon} alt="mail sent" height="80px"/>
+    <div className="application__after">
+     <h1 className="application__after--title">Application sent</h1>
+     <p className="application__after--description">We are reviewing your application.</p>
+     <p className="application__after--description">We will email you soon.
+     </p>
+     <div className="application__after--image-box">
+      <img src={mailIcon} alt="mail sent" height="80px"/>
+     </div>
     </div>
   )
 }

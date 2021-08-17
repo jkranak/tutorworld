ab4f3c139fef31b6b235dc68d67ae3ce8e24903c
Rahmat <rahmat.allah.yousufi@gmail.com>
2021-08-16 20:33:00 -0400

    feat: role middlewares for extra protection, added missed edge case in createStudent and login controllers

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -9,7 +9,9 @@ export const createStudent = async (req:any, res:any) => {
 
   try {
     const user = await Models.Student.findOne({where: {email}});
-    if (user) {
+    const tutor = await Models.Tutor.findOne({where: {email}});
+
+    if (user || tutor ) {
       return res.status(400).send({ message: 'Email taken, chose another one.' });
     }
     if (password !== confirmPassword) {
@@ -23,7 +25,7 @@ export const createStudent = async (req:any, res:any) => {
         id: newUser.id,
         role: 'student',
       },
-      token: generateToken(newUser.id),
+      token: generateToken(newUser.id, 'student'),
     });
   } catch (error) {
     console.log(error)
@@ -55,7 +57,7 @@ export const login = async (req:any, res:any) => {
             id: user.id,
             role: user===student? 'student':'tutor',
           },
-          token: generateToken(user.id),
+          token: generateToken(user.id, user===student? 'student':'tutor'),
         },
       );
     } else {
@@ -74,7 +76,8 @@ export const createTutor = async (req:any, res:any) => {
 
   try {
     const user = await Models.Tutor.findOne({where: {email}});
-    if (user) {
+    const student = await Models.Student.findOne({where: {email}});
+    if (user || student) {
       return res.status(400).send({ message: 'Email taken, chose another one.' });
     }
     if (password !== confirmPassword) {

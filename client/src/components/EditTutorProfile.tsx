35461e987e3e7ff357b1c5b1a79b6179af1ce48a
biancahpp <bianca_procopio@hotmail.com>
2021-08-17 19:56:46 -0600

    feat: basic style for edit tutor profile

--- client/src/components/EditTutorProfile.tsx
+++ client/src/components/EditTutorProfile.tsx
@@ -43,43 +43,58 @@ export const EditTutorProfile: FC<Props> = ({setEditing, tutor }: Props) => {
   }
 
   return (
-    <div>
+    <div className="form edit-form">
       <form onSubmit={handleSubmit}>
-        <label>Change First Name</label>
-        <input type="text" id="fname" name="firstName" onChange={handleTutorChange} value={tutor?.firstName} placeholder="First Name" required/>
-        <label>Change Last Name</label>
-        <input type="text" id="lname" name="lastName" onChange={handleTutorChange}  value={tutor?.lastName} placeholder="Last Name"required/>
-        <label>Change Email</label>
-        <input type="email" id="email" name="email" onChange={handleTutorChange} value={tutor?.email} placeholder="Email" required/>
-        <label>Change Brief Description</label>
-        <input type="text" id="description" name="description" onChange={handleTutorChange} value={tutor?.description} placeholder="Description" required/>
-        <label>Change Hourly Rate [$/hour]</label>
-        <input type="number" min="0" id="price" name="price" onChange={handleTutorChange} value={tutor?.price} placeholder="Hourly Rate $/hr" required/>
-        <label>Change Education</label>
-        <input type="text" id="education" name="education" onChange={handleTutorChange} value={tutor?.education} placeholder="Education" required/>
-        <select name="subjects" onChange={handleSelect} defaultValue="Add subjects" >
-          <option value="Add subjects" disabled hidden>Add subjects</option>
+        <div className="form--title">
+          <h1>Edit Profile</h1>
+        </div>
+
+        <label>First Name</label>
+        <input type="text" id="fname" name="firstName" onChange={handleTutorChange} value={tutor?.firstName} placeholder="First Name" required className="text-input text-input--blue"/>
+
+        <label>Last Name</label>
+        <input type="text" className="text-input text-input--blue" id="lname" name="lastName" onChange={handleTutorChange}  value={tutor?.lastName} placeholder="Last Name"required/>
+
+        <label>Email</label>
+        <input type="email" id="email" name="email" onChange={handleTutorChange} value={tutor?.email} placeholder="Email" required className="text-input text-input--blue"/>
+
+        <label>Description</label>
+        <input type="text" className="text-input text-input--blue" id="description" name="description" onChange={handleTutorChange} value={tutor?.description} placeholder="Description" required/>
+
+        <label>Hourly Rate [$/hour]</label>
+        <input type="number" min="0" id="price" name="price" onChange={handleTutorChange} value={tutor?.price} placeholder="Hourly Rate $/hr" required className="text-input text-input--blue"/>
+
+        <label>Education</label>
+        <textarea className="text-input text-input--blue text-input--textarea" id="education" name="education" onChange={handleTutorChange} value={tutor?.education} placeholder="Education" required/>
+
+        <select name="subjects" onChange={handleSelect} defaultValue="" className="select-input select-input--blue">
+          <option value="" disabled>Add subjects</option>
           {subjects.map((subject, index) => (
             <option key={index} value={subject}>{subject}</option>
           ))}
         </select>
-        {tutor?.subjectLevels.map((subject, index): any => 
-          <div key={subject}><span>{subject}</span>
-            {tutor?.subjectLevels[0].length > 0 && <FiX onClick={() => removeSubject(index)}/>}
-          </div>)}
-        <select name="languages" onChange={handleSelect} defaultValue="Add languages" >
-          <option value="Add languages" disabled hidden>Add Languages</option>
+            <div className="form--multi-select">
+              {tutor?.subjectLevels.map((subject, index): any => 
+                <div key={subject}><span className="form--select-tag">{subject}</span>
+                  {tutor?.subjectLevels[0].length > 0 && <FiX onClick={() => removeSubject(index)}/>}
+                </div>)}
+            </div>
+        <select name="languages" onChange={handleSelect} defaultValue="" className="select-input select-input--blue" >
+          <option value="" disabled>Add Languages</option>
           {languages.map((language, index) => (
             <option key={index} value={language}>{language}</option>
           ))}
         </select>
+        <div className="form--multi-select">
         {tutor?.languages.map((language, index): any => 
-          <div key={language}><span>{language}</span>
+          <div key={language}><span className="form--select-tag" >{language}</span>
             {tutor?.languages[0].length > 0 && <FiX onClick={() => removeLanguage(index)}/>}
           </div>)}
+
+        </div>
           <label>Change Experience</label>
-          <input type="text" id="experience" name="experience" onChange={handleTutorChange} value={tutor?.experience} placeholder="Experience" required/>
-          <button type="submit">Submit</button>
+          <textarea className="text-input text-input--blue text-input--textarea" id="experience" name="experience" onChange={handleTutorChange} value={tutor?.experience} placeholder="Experience" required/>
+          <button type="submit" className="btn btn--blue form--btn">Submit</button>
       </form>
     </div>
   )

679dab06b466487e7c8707e13896687c49892f77
Rahmat <rahmat.allah.yousufi@gmail.com>
2021-08-18 19:12:11 -0400

    feat: getTutorInfoAvail & addFavorTutor route/controller implemented, half complete getAllFavTutors controller

--- server/src/controllers/favTutorsController.ts
+++ server/src/controllers/favTutorsController.ts
@@ -1,7 +1,20 @@
 import Models from '../../models';
 
 export const addFavTutor = async (req:any, res:any) => {
+  try {
+    const { id  } = req.body.user;
 
+    const { TutorId  } = req.body;
+
+    await Models.FavTutor.create({TutorId, StudentId: id});
+
+    res.status(201).send('Added as a favourite tutor')
+
+  } catch (error) {
+    console.log(error)
+    res.status(500);
+    res.send(error);
+  }
 
 }
 
@@ -10,7 +23,22 @@ export const removeFavTutor = async (req:any, res:any) => {
 
 }
 
-export const getAllFavTutor = async (req:any, res:any) => {
+export const getAllFavTutors = async (req:any, res:any) => {
+  try {
+    const { id  } = req.body.user;
 
+    const allFavTutorsInstance = await Models.FavTutor.findAll({attributes: {include: ['TutorId']}});
+
+    const allFavTutorsId = allFavTutorsInstance.map((FavTutorInstance:any) => FavTutorInstance.get({plain: true }));
+    //send back tutorId , first name last name
+    console.log(allFavTutorsId)
+
+    res.status(201).send(allFavTutorsId)
+
+  } catch (error) {
+    console.log(error)
+    res.status(500);
+    res.send(error);
+  }
 
 }

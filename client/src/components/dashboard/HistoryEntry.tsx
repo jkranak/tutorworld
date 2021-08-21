6b90eafc88a2f9432c0131ea5741564468ea9c5d
Joseph Kranak <jakranak@gmail.com>
2021-08-21 11:06:28 -0500

    feat: upcoming sessions on dashboard sorted from most to least recent

--- client/src/components/dashboard/HistoryEntry.tsx
+++ client/src/components/dashboard/HistoryEntry.tsx
@@ -16,6 +16,13 @@ interface Props {
 export const HistoryEntry: FC<Props> = ({session, user}: Props) => {
   const [otherUserInfo, setOtherUserInfo] = useState(emptyUserNameImage);
   const starArr: number[] = starRatingWhole(session.starRating);
+  const reviewState = {
+      date: session.date,
+      time: session.time,
+      name: `${otherUserInfo.firstName} ${otherUserInfo.lastName}`,
+      rating: session.starRating ? session.starRating : 0,
+      review: session.review ? session.review : ''
+    }
 
   useEffect(() => {
     if (session.StudentId && session.TutorId) {
@@ -32,18 +39,16 @@ export const HistoryEntry: FC<Props> = ({session, user}: Props) => {
       <p>{otherUserInfo.imageUrl 
           ? <img src={otherUserInfo.imageUrl} alt={`${otherUserInfo.firstName} ${otherUserInfo.lastName}`} height="40px" />
           : <img src={noPhotoUser} alt={`${otherUserInfo.firstName} ${otherUserInfo.lastName}`} height="40px" />}
-        {otherUserInfo.firstName} {otherUserInfo.lastName} - {session.date}, {session.time} - Price: ${session.cost} - {session.review} - 
+        {otherUserInfo.firstName} {otherUserInfo.lastName} - {session.date}, {session.time} - Price: ${session.cost} - 
+        {user.role === 'student' ? <Link to={{
+            pathname:'/review', 
+            state: reviewState
+          }}> Review</Link> : <span> Review</span>}: {session.review} - 
         {session.starRating > 0 ? <span>{starArr?.map(el => (
             el === 2 ? <BsStarFill key={uuidv4()} className="tutor-profile__info--star"/> : <BsStar key={uuidv4()} className="tutor-profile__info--star"/>
           ))}</span> : user.role === 'tutor' ? <span>No ratings yet</span> : <Link to={{
             pathname:'/review', 
-            state:{
-              date: session.date,
-              time: session.time,
-              name: `${otherUserInfo.firstName} ${otherUserInfo.lastName}`,
-              rating: session.starRating ? session.starRating : 0,
-              review: session.review ? session.review : ''
-            }
+            state: reviewState
           }}>Review this session</Link>}
       </p>
     </div>

f43924285075f2ce74cc39461ccf83a0e69578c2
biancahpp <bianca_procopio@hotmail.com>
2021-08-21 18:12:44 -0600

    feat: style for dashboard complete

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import { FaCalendar, FaComment, FaUserAlt } from 'react-icons/fa';
+import { FaCalendar, FaClock, FaComment, FaUserAlt } from 'react-icons/fa';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 
@@ -9,7 +9,7 @@ export const Sidebar: FC = () => {
   return (
     <div className="sidebar">
       <Link to={'/profile'} className="btn btn--clear"><FaUserAlt className="sidebar__icon"/> Profile</Link>
-      {user.role === 'tutor' && <button className="btn btn--clear">Availability</button>}
+      {user.role === 'tutor' && <button className="btn btn--clear"><FaClock className="sidebar__icon"/>Availability</button>}
       <Link to={'/messages'} className="btn btn--clear"><FaComment className="sidebar__icon"/>Conversations</Link>
       <button className="btn btn--clear" ><FaCalendar className="sidebar__icon"/>Calendar</button>
     </div>

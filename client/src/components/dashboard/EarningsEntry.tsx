d862b8b9b9e0494ea66528a27da845ee472a3c87
biancahpp <bianca_procopio@hotmail.com>
2021-08-25 09:23:05 -0600

    feat: earnings absic style done

--- client/src/components/dashboard/EarningsEntry.tsx
+++ client/src/components/dashboard/EarningsEntry.tsx
@@ -1,3 +1,4 @@
+import moment from 'moment'
 import { HistoryComplex } from '../../interfaces/Session'
 
 interface Props {
@@ -7,9 +8,9 @@ export default function EarningsEntry({ session }: Props) {
   return (
     <div className="dashboard__content--display--session">
       <div className="dashboard__content--display--session-details">
-        <div className="dashboard__content--display--session--left-box">
-          <h2>${session.cost}</h2>
-          <h2>{session.date}</h2>
+        <div className="dashboard__content--display--session--left-box earnings">
+          <h2 className="earnings-date">Date: {moment(session.date).format('MMMM DD, YYYY')}</h2>
+          <h2 className="earnings-cost">Earning: ${session.cost}</h2>
         </div>
       </div>
     </div>

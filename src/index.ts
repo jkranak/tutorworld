545e08bf8c515be93d88105fe88c276fb337f974
Priyanshu Tiwari <tpriyanshu90@gmail.com>
2021-09-07 19:38:43 +0530

    deployment completed

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ const app = express();
 const path = require('path');
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('build'));
+  app.use(express.static(path.join(__dirname, 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html')); // relative path
   });
@@ -19,8 +19,8 @@ if (process.env.NODE_ENV === 'production') {
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(router);
+
 const server = app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
 
 (() => {
